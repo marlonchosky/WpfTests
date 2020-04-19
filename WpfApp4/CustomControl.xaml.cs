9e0b4acf85e7c1649261fa32c7521a9ce768235a
Marlon Eduardo Peña Llanos <marlonedu.pe@gmail.com>
2020-04-18 20:30:26 -0500

    Test

---
+++ WpfApp4/CustomControl.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp4 {
+    /// <summary>
+    /// Interaction logic for CustomControl.xaml
+    /// </summary>
+    public partial class CustomControl : UserControl {
+        public static readonly DependencyProperty FirstNameProperty = DependencyProperty.Register(
+            "FirstName",
+            typeof(string),
+            typeof(CustomControl),
+            new FrameworkPropertyMetadata(default(string), FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        public string FirstName {
+            get {
+                return (string)GetValue(FirstNameProperty);
+            }
+            set {
+                SetValue(FirstNameProperty, value);
+            }
+        }
+
+        public CustomControl() {
+            InitializeComponent();
+        }
+    }
+}
---
+++ WpfApp4/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp4 {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+            this.DataContext = new MainViewModel();
+        }
+    }
+
+    public class MainViewModel : INotifyPropertyChanged {
+        private string _test;
+        public string Test {
+            get { return _test; }
+            set {
+                _test = value;
+                //OnPropertyChanged(string.Empty);
+                OnPropertyChanged("Test");
+            }
+        }
+
+        public MainViewModel() {
+            Test = "abcddd";
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}

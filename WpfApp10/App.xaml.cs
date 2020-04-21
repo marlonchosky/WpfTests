7b0a127e4d734b140c42aab052efbdc1d12f1925
Marlon Eduardo Peña Llanos <marlonedu.pe@gmail.com>
2020-04-21 12:51:26 -0500

    Test

---
+++ WpfApp10/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace WpfApp10 {
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application {
+        private void App_OnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e) {
+            MessageBox.Show(e.Exception.Message);
+            e.Handled = true;
+        }
+    }
+}
---
+++ WpfApp10/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp10 {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+            var vm = new TestViewModel();
+            this.DataContext = vm;
+            //throw new Exception("Test2 Exception");
+        }
+
+        private void Validation_OnError(object sender, ValidationErrorEventArgs e) {
+            throw e.Error.Exception;
+        }
+    }
+
+    public class TestViewModel {
+        private string testProp;
+
+        public string TestProp {
+            get => testProp;
+            set {
+                testProp = value;
+
+                // RAISE EXCEPTION IN PROPERTY SETTER
+                throw new Exception("Test Exception");
+            }
+        }
+    }
+}

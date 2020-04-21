4853f766a32f6237c805b7d9aeb0ba050fd4feb8
Marlon Eduardo Peña Llanos <marlonedu.pe@gmail.com>
2020-04-21 16:53:56 -0500

    Test

---
+++ WpfApp12/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp12 {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void OpenWindow_OnClick(object sender, RoutedEventArgs e) {
+            var windowToOpen = new Window1();
+            windowToOpen.Closing += (o, args) => {
+            };
+            windowToOpen.AfterClosingEvent += (o, s) => {
+                this.TextBlock1.Text = s;
+            };
+            windowToOpen.Show();
+        }
+    }
+}
---
+++ WpfApp12/Window1.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp12 {
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window {
+        public Window1() {
+            InitializeComponent();
+        }
+
+        public event EventHandler<string> AfterClosingEvent;
+
+        private void Window1_OnClosing(object sender, CancelEventArgs e) {
+            // Logic for your database update...
+
+            this.AfterClosingEvent?.Invoke(this, this.TextBox1.Text);
+        }
+    }
+}

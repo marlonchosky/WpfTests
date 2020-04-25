d65a3e7d0d1e28390b7da05247aece63adf097cf
Marlon Eduardo Peña Llanos <marlonedu.pe@gmail.com>
2020-04-24 22:14:55 -0500

    Test

---
+++ WpfApp16/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp16 {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void GetPosition_OnClick(object sender, RoutedEventArgs e) {
+            Point posicion = Mouse.GetPosition(Application.Current.MainWindow);
+            TextPointer posAhora = richTextBox1.GetPositionFromPoint(posicion, false);
+            if (posAhora != null) {
+                check.IsChecked = true;
+            } else {
+                check.IsChecked = false;
+            }
+        }
+    }
+}

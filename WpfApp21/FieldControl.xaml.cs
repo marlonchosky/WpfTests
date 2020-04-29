a03d9467bbb155c00679a2b3beb39e7e65aedfb5
Marlon Eduardo Peña Llanos <marlonedu.pe@gmail.com>
2020-04-29 13:29:30 -0500

    Test

---
+++ WpfApp21/FieldControl.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp21 {
+    /// <summary>
+    /// Interaction logic for FieldControl.xaml
+    /// </summary>
+    public partial class FieldControl : UserControl {
+        #region BackgroundPath Dependency Property
+        public string BackgroundPath {
+            get {
+                return (string)GetValue(BackgroundPathProperty);
+            }
+            set {
+                SetValue(BackgroundPathProperty, value);
+            }
+        }
+
+        public static readonly DependencyProperty BackgroundPathProperty =
+            DependencyProperty.Register("BackgroundPath", typeof(string), typeof(FieldControl), new PropertyMetadata(null));
+
+        #endregion
+        public FieldControl() {
+            InitializeComponent();
+        }
+    }
+}
---
+++ WpfApp21/StringToImageSourceConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace WpfApp21 {
+    [ValueConversion(typeof(string), typeof(ImageSource))]
+    public class StringToImageSourceConverter : IValueConverter {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            if (value == null) return null;
+
+            var path = value.ToString();
+            return new BitmapImage(new Uri(path, UriKind.RelativeOrAbsolute));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            throw new NotSupportedException();
+        }
+    }
+}

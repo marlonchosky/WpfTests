223fbf24f6196142aa8b4c5fff225c6c113b7043
Marlon Eduardo Peña Llanos <marlonedu.pe@gmail.com>
2020-04-29 12:53:11 -0500

    Test

---
+++ WpfApp19/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DevExpress.Mvvm.DataAnnotations;
+using DevExpress.Mvvm.POCO;
+using DevExpress.Xpf.Editors;
+
+namespace WpfApp19 {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+            this.DataContext = Vm.Create();
+        }
+    }
+
+    [POCOViewModel]
+    public class Vm {
+        public virtual bool TickCheckBox { get; set; } = false;
+        public virtual EditMode EditMode { get; set; } = EditMode.InplaceInactive;
+
+        public static Vm Create() => ViewModelSource.Create(() => new Vm());
+
+        protected void OnTickCheckBoxChanged() {
+            if (this.TickCheckBox) {
+                this.EditMode = EditMode.Standalone;
+            } else {
+                this.EditMode = EditMode.InplaceInactive;
+            }
+        }
+    }
+}
---
+++ WpfApp20/Window1.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp20 {
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window {
+        public Window1() {
+            InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e) {
+            var element = this.GetElementInGridPosition(1, 1);
+            if (element is Button)
+                MessageBox.Show($"The element in 1,1 is a button.");
+
+            element = this.GetElementInGridPosition(2, 1);
+            if (element is Button)
+                MessageBox.Show($"The element in 2,1 is a button.");
+            else
+                MessageBox.Show($"The element in 2,1 isn't a button, it's a {element.GetType().Name}.");
+        }
+
+        private UIElement GetElementInGridPosition(int column, int row) {
+            foreach (UIElement element in this.RootGrid.Children) {
+                if (Grid.GetColumn(element) == column && Grid.GetRow(element) == row)
+                    return element;
+            }
+
+            return null;
+        }
+    }
+}

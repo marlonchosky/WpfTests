4bacbe06785442284ff776f80578f672f01b4870
Marlon Eduardo Peña Llanos <marlonedu.pe@gmail.com>
2020-05-02 11:01:39 -0500

    Test

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2 {
+    class Program {
+        static void Main(string[] args) {
+            var dir = new DirectoryInfo(@"C:\Windows\Web\Wallpaper");
+            var imageFiles = dir.GetFiles("*.jpg", SearchOption.AllDirectories);
+
+            foreach (var f in imageFiles) {
+                Console.WriteLine($"FullName: {f.FullName}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}

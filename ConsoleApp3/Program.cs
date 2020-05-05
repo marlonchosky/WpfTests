e6610a90271380248234263eba539417c2625680
Marlon Eduardo Peña Llanos <marlonedu.pe@gmail.com>
2020-05-05 12:54:09 -0500

    Test

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3 {
+    class StatusChecker {
+        private int numberOfSecondsLeft;
+
+        public StatusChecker(int numberOfSecondsLeft) {
+            this.numberOfSecondsLeft = numberOfSecondsLeft;
+        }
+
+        public void CountDown(object state) {
+            Console.Write($"{this.numberOfSecondsLeft - 1},");
+
+            var autoEvent = (AutoResetEvent)state;
+            if (--this.numberOfSecondsLeft == 0) {
+                autoEvent.Set();
+            }
+        }
+    }
+
+    class Program {
+        static void Main(string[] args) {
+            var numberOfSeconds = input();
+            var autoEvent = new AutoResetEvent(false);
+
+            var checker = new StatusChecker(numberOfSeconds + 1);
+            Console.WriteLine("Time left:");
+            var t = new Timer(checker.CountDown, autoEvent, 0, 1000);
+            autoEvent.WaitOne();
+            t.Dispose();
+        }
+
+        public static int input() {
+            int numberOfSeconds;
+            do {
+                Console.WriteLine("How many seconds would you like the test to be? Please type a number divisible by 10!");
+                int.TryParse(Console.ReadLine(), out numberOfSeconds);
+            } while (numberOfSeconds % 10 != 0);
+            return numberOfSeconds;
+        }
+    }
+}
---
+++ WpfApp25/MainWindow.xaml.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using FluentValidation;
+
+namespace WpfApp25 {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        DispatcherTimer timer;
+        TimeSpan time;
+
+        public MainWindow() {
+            InitializeComponent();
+
+            var executeQueries = new List<Func<CancellationToken, Task<ICollection<Group>>>>();
+
+            var domains = new List<string> {
+                "domain 1", "domain 2"
+            };
+
+            domains.ForEach(domain =>
+                executeQueries.Add(async (ct) =>
+                    await ExecuteQueryGroupsForDomain(domain, 123, ct)));
+
+            Expression<Func<CancellationToken, string, Task<ICollection<Group>>>> expr = (token, domain) =>
+                this.ExecuteQueryGroupsForDomain(domain, 123, token);
+
+            var abc = expr.Compile()(CancellationToken.None, "abc");
+
+            foreach (var domain in domains) {
+                executeQueries.Add(token => this.ExecuteQueryGroupsForDomain(domain, 123, token));
+            }
+            executeQueries.AddRange(domains
+                .Select(domain => (Func<CancellationToken, Task<ICollection<Group>>>) (token => 
+                    this.ExecuteQueryGroupsForDomain(domain, 123, token))));
+            executeQueries =
+                (from domain in domains
+                select new Func<CancellationToken, Task<ICollection<Group>>>(token => 
+                    this.ExecuteQueryGroupsForDomain(domain, 123, token))).ToList();
+
+
+
+
+            //var executeQueries2 = domains.Select(domain =>
+            //    (async (ct) => await ExecuteQueryGroupsForDomain(domain, 123, ct)));
+            //var executeQueries2 = domains.Select(domain =>
+            //    (async (CancellationToken ct) => { return await ExecuteQueryGroupsForDomain(domain, 123, ct); }));
+
+            executeQueries = domains.Select(domain => 
+                new Func<CancellationToken, Task<ICollection<Group>>>(token => 
+                    this.ExecuteQueryGroupsForDomain(domain, 123, token))).ToList();
+            executeQueries = domains.Select<string, Func<CancellationToken, Task<ICollection<Group>>>>(domain =>
+                ct => this.ExecuteQueryGroupsForDomain(domain, 123, ct)).ToList();
+                
+
+
+
+            //MessageBox.Show($"{int.Parse(outS)}");
+
+
+            //MessageBox.Show($"{int.Parse(outS)}\n" +
+            //                $"{Metodo1(length)}");
+        }
+
+
+        private async Task<ICollection<Group>> ExecuteQueryGroupsForDomain(string domain, int i, CancellationToken ct) {
+            return new List<Group> {
+                new Group()
+            };
+        }
+
+        private int Metodo1(int length) => new Random().Next((int)Math.Pow(10, length - 1), (int)Math.Pow(10, length));
+
+        private int Metodo2(int length) {
+            var outS = "";
+            var random = new Random();
+            for (int i = 0; i < length; i++) {
+                outS += random.Next(0, 9).ToString();
+            }
+
+            return int.Parse(outS);
+        }
+
+        //private async void AppearingNext() {
+        //    await Task.Delay(TimeSpan.FromSeconds(10));
+        //    VisibilityPanel.Visibility = Visibility.Visible;
+
+        //}
+    }
+
+    public class Group { }
+
+    public class MyClassToValidate {
+        public string Name { get; set; }
+    }
+
+    public class MyClassToValidateValidator : AbstractValidator<MyClassToValidate> {
+        public MyClassToValidateValidator(IMyClassToValidateRepository repository) {
+            RuleFor(x => x.Name)
+                .MustAsync(async (name, ct) => 
+                    (await repository.GetDataAsync()).All(x => x.Name != name));
+        }
+    }
+
+    public interface IMyClassToValidateRepository {
+        Task<IList<MyClassToValidate>> GetDataAsync();
+    }
+}
